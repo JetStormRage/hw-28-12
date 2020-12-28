a94a19f4686a8e14302000837968e8f7e8501e79
JetStormRage <75043067+JetStormRage@users.noreply.github.com>
2020-12-28 16:32:38 +0300

    Add files via upload

---
+++ js/cart.js
@@ -0,0 +1,12 @@
+// Це мало б зробити щоб кнопочка + додавала 1 до значення кількості=(
+function plus() {
+   document.querySelector('.quantity')
+    return ('.quantity') + 1;
+}
+const buttonPlus = document.querySelector('.button-plus')
+buttonPlus.addEventListener('click', plus);
+
+
+function sum() {
+    Math.sum() '.quantity' * '.price';
+}
